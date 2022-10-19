c8f13c8fb5731f58800acbb4c4c61219d9a60e64
rahulnagulakonda <rahul.nagulakonda@proeffective.in>
2022-10-19 21:14:14 +0530

    exposing header for cors

--- src/main/java/com/interon/admin/interceptor/InterceptorConfiguration.java
+++ src/main/java/com/interon/admin/interceptor/InterceptorConfiguration.java
@@ -19,6 +19,6 @@ public class InterceptorConfiguration implements WebMvcConfigurer{
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/**").exposedHeaders("*").allowedMethods("*");
 	}
 }

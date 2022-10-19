24a8415ebfe2db3161a100071d72bb8fd8426881
rahulnagulakonda <rahul.nagulakonda@proeffective.in>
2022-10-19 23:23:20 +0530

    specifying only angular origin

--- src/main/java/com/interon/admin/interceptor/CorsConfiguration.java
+++ src/main/java/com/interon/admin/interceptor/CorsConfiguration.java
@@ -2,9 +2,11 @@ package com.interon.admin.interceptor;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
@@ -12,7 +14,7 @@ public class CorsConfiguration implements WebMvcConfigurer{
 		.addMapping("/**")
 		.allowedHeaders("*","Access-Control-Allow-Origin","Access-Control-Allow-Methods","Access-Control-Allow-Headers")
 		.allowedMethods("*")
-		.allowedOrigins("*")
+		.allowedOrigins("http://localhost:4200")
 		.exposedHeaders("*","Access-Control-Allow-Origin","Access-Control-Allow-Methods","Access-Control-Allow-Headers");
 	}
 	

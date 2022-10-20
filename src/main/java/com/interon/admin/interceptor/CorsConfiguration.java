c6fcbc435b8b46699d39ffd21c82d22f23495e4e
rahulnagulakonda <rahul.nagulakonda@proeffective.in>
2022-10-20 12:53:00 +0530

    cors filter

--- src/main/java/com/interon/admin/interceptor/CorsConfiguration.java
+++ src/main/java/com/interon/admin/interceptor/CorsConfiguration.java
@@ -1,26 +1,20 @@
-package com.interon.admin.interceptor;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfiguration {
-	
-	@Bean
-	public WebMvcConfigurer getCorsConfiguration() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry
-				.addMapping("/**")
-				.allowedOrigins("http://localhost:4200")
-				.allowedMethods("*")
-				.allowedHeaders("*","Access-Control-Allow-Origin")
-				.exposedHeaders("*","Access-Control-Allow-Origin");
-			}
-		};
-	}
-	
-}
+/*
+ * package com.interon.admin.interceptor;
+ * 
+ * import org.springframework.context.annotation.Bean; import
+ * org.springframework.context.annotation.Configuration; import
+ * org.springframework.web.servlet.config.annotation.CorsRegistry; import
+ * org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ * 
+ * @Configuration public class CorsConfiguration {
+ * 
+ * @Bean public WebMvcConfigurer getCorsConfiguration() { return new
+ * WebMvcConfigurer() {
+ * 
+ * @Override public void addCorsMappings(CorsRegistry registry) { registry
+ * .addMapping("/**") .allowedOrigins("http://localhost:4200")
+ * .allowedMethods("*") .allowedHeaders("*","Access-Control-Allow-Origin")
+ * .exposedHeaders("*","Access-Control-Allow-Origin"); } }; }
+ * 
+ * }
+ */

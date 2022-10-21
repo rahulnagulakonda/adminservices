f142046036a961e85f405d7575d0a2cd2340faab
rahulnagulakonda <rahul.nagulakonda@proeffective.in>
2022-10-21 17:53:37 +0530

    terraform update

--- deploy/dev/main.tf
+++ deploy/dev/main.tf
@@ -2,6 +2,8 @@ provider "aws" {
     region = "us-east-1"
 }
 
+variable "docker_image_version" {}
+
 module "my_ecs_dev" {
 	source = "../modules"
 	env = "dev"
--- deploy/qa/main.tf
+++ deploy/qa/main.tf
@@ -2,6 +2,8 @@ provider "aws" {
     region = "us-east-1"
 }
 
+variable "docker_image_version" {}
+
 module "my_ecs_qa" {
 	source = "../modules"
 	env = "qa"
